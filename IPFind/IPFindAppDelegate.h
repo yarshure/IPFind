d1c8fc176ed005ce93a906fb27673ac71c6774b8
yarshure <kongxiangbo@snda.com>
2011-09-29 15:31:11 +0800

    Initial Commit

---
+++ IPFind/IPFindAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IPFindAppDelegate.h
+//  IPFind
+//
+//  Created by XiangBo Kong on 11-9-29.
+//  Copyright 2011年 macdevshanghai.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IPFindAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ IPFind/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  IPFind
+//
+//  Created by XiangBo Kong on 11-9-29.
+//  Copyright 2011年 macdevshanghai.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
